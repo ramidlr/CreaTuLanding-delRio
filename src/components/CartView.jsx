b69628e4c07f9bbd1770c47563709b1c060a47e5
Ramiro del Rio <ramirodelrio@Ramiros-MacBook-Pro.local>
2025-07-10 02:23:26 +0200

    sweetalert import, aplicacion de mensaje de alerta en vaciar carrito.

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -2,9 +2,25 @@ import  '../css/CartView.css'
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import Swal from 'sweetalert2'
 
 const CartView = () => {
     const { cart, deleteCart, removeItem, finalPrice } = useContext(CartContext);
+    const preConfirm = () => {
+        Swal.fire({
+            title: 'Estas seguro que queres borrar el carrito?',
+            showDenyButton:true,
+            confirmButtonText: 'Si',
+            denyButtonText: 'No'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteCart()
+                Swal.fire('Carrito borrado', '', 'success')
+            }else if (result.isDenied) {
+                Swal.fire('No se borro el carrito', '', 'info')
+            }
+        })
+    }
     return (
         <div className="d-flex flex-column align-items-center text-center min-vh-100">
             <h2 className="fw-bold mt-4 mb-4">Ya casi has finalizado tu compra</h2>
@@ -33,7 +49,7 @@ const CartView = () => {
             <span className="fw-bold">Total a pagar: ${finalPrice()}</span>
             <div className='text-center p-3 border rounded mb-4 mt-4'>
                 <Link className="btn btn-success me-2" to="/checkout">Terminar compra</Link>
-                <button className="btn btn-danger" onClick={deleteCart}>Vaciar carrito</button>
+                <button className="btn btn-danger" onClick={preConfirm}>Vaciar carrito</button>
             </div>
         </div>
     );
